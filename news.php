2f5187c519682800491940b169f2ad3654492fec
Tileoplayer <maginetweb@gmail.com>
2025-12-18 08:35:18 +0100

    add file

---
+++ news.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: #475569;
+}
+</style>
+</head>
+<body>
+
+<div class="w3-container w3-center">
+<h4>Ascolti ieri in TV</h4>
+</div>
+
+<div class="w3-container w3-center">
+<h4>gli ascolti di ieri (Rai televideo)</h4>
+</div>
+
+<img src="//www.servizitelevideo.rai.it/televideo/pub/tt4web/Nazionale/16_9_page-533.png" alt="533-pag1" style="width:100%">
+<hr>
+<img src="//www.servizitelevideo.rai.it/televideo/pub/tt4web/Nazionale/16_9_page-533.2.png" alt="533-pag2" style="width:100%">
+<hr>
+
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
---
+++ resources.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: #475569;
+}
+</style>
+</head>
+<body>
+
+<div class="w3-container w3-center">
+<h4>Stasera in TV</h4>
+</div>
+
+<div class="w3-container w3-red">
+  <h3>la prima serata delle più importanti tv italiane</h3>
+
+
+
+<iframe src="#"></iframe></p></div>
+
+
+
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
